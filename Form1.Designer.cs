4eea237224c93cc9b2d9520d7fa782c745f6d5bf
R3LOAD3D <amin.mohseni@outlook.com>
2014-10-15 13:32:35 +0200

    After Gitignore
    Gitignore

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,28 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.newgame = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // newgame
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(529, 14);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 63);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "New Game";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.newgame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newgame.Location = new System.Drawing.Point(529, 14);
+            this.newgame.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.newgame.Name = "newgame";
+            this.newgame.Size = new System.Drawing.Size(94, 63);
+            this.newgame.TabIndex = 1;
+            this.newgame.Text = "New Game";
+            this.newgame.UseVisualStyleBackColor = true;
+            this.newgame.Click += new System.EventHandler(this.button1_Click);
             // 
             // button3
             // 
@@ -68,29 +70,9 @@
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Location = new System.Drawing.Point(12, 106);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(517, 482);
+            this.panel1.Size = new System.Drawing.Size(480, 480);
             this.panel1.TabIndex = 5;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
-            // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.textBox1.Location = new System.Drawing.Point(638, 220);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 30);
-            this.textBox1.TabIndex = 6;
-            this.textBox1.Text = " stenen";
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.textBox2.Location = new System.Drawing.Point(638, 131);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 30);
-            this.textBox2.TabIndex = 7;
-            this.textBox2.Text = "stenen";
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // panel2
             // 
@@ -115,18 +97,58 @@
             this.panel4.TabIndex = 0;
             this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
             // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textBox3.Location = new System.Drawing.Point(542, 326);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(195, 30);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "X-speler aan zet";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textBox4.Location = new System.Drawing.Point(542, 390);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(195, 30);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "Winnaar - Game";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(655, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(655, 225);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(750, 600);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.newgame);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
@@ -140,14 +162,16 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button newgame;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,16 +16,67 @@ namespace WindowsFormsApplication4
         int gridvaksize = 60;
         //int xPanel = 12; -> voor coordinatie grid
         //int yPanel = 106; -> ""
+        string speleraanzet = "Blauw";
+        int[,] tegels = new int[8, 8];
+
+
+
 
 
         public Form1()
         {
             InitializeComponent();
 
+            //abonnementen op events
             panel1.Paint += new PaintEventHandler(panel1_Paint);
             panel3.Paint += new PaintEventHandler(panel3_Paint);
+            panel1.MouseClick += panel1_MouseClick;
 
 
+
+                
+        }
+
+
+        private bool ValidMove(int MouseX, int MouseY)
+        {
+            if(tegels[MouseX, MouseY] == 0)
+            {
+                return true;
+
+            }
+            return false;
+
+
+
+        } 
+
+        void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+           int MouseX = e.X / gridvaksize; //x-coördinaten muis
+           int MouseY = e.Y / gridvaksize; //y-coördinaten muis
+          
+           Graphics gr = this.panel1.CreateGraphics();
+           if (speleraanzet == "Blauw")
+           {
+               if (ValidMove(MouseX, MouseY))
+               {
+                   tegels[MouseX, MouseY] = 1;
+                   gr.FillEllipse(Brushes.Blue, MouseX * gridvaksize, MouseY * gridvaksize, gridvaksize, gridvaksize);
+                   speleraanzet = "Rood";
+               }
+
+           }
+           else
+           {
+               if (ValidMove(MouseX, MouseY))
+               {
+                   tegels[MouseX, MouseY] = -1;
+                   gr.FillEllipse(Brushes.Red, MouseX * gridvaksize, MouseY * gridvaksize, gridvaksize, gridvaksize);
+                   speleraanzet = "Blauw";
+               }
+           }
+           Label1_Teller();
         }
 
 
@@ -34,12 +85,15 @@ namespace WindowsFormsApplication4
             Pen myPen = new Pen(Color.Black);
             myPen.Width = 3;
 
-
+            e.Graphics.FillEllipse(Brushes.Blue, numvak / 2 * gridvaksize, numvak / 2 * gridvaksize, gridvaksize, gridvaksize);
+            e.Graphics.FillEllipse(Brushes.Blue, numvak / 2 * gridvaksize - gridvaksize, numvak / 2 * gridvaksize - gridvaksize, gridvaksize, gridvaksize);
+            e.Graphics.FillEllipse(Brushes.Red, numvak / 2 * gridvaksize, numvak / 2 * gridvaksize - gridvaksize, gridvaksize, gridvaksize);
+            e.Graphics.FillEllipse(Brushes.Red, numvak / 2 * gridvaksize - gridvaksize, numvak / 2 * gridvaksize, gridvaksize, gridvaksize);
             for (int y = 0; y <= numvak; y++)
             {
                 e.Graphics.DrawLine(myPen, y * gridvaksize, 0, y * gridvaksize, numvak * gridvaksize);  //Vertical Grid Lines
                 e.Graphics.DrawLine(myPen, 0, y * gridvaksize, gridvaksize * numvak, y * gridvaksize); // Horizontal Grid Lines
-
+               
             }
         }
 
@@ -55,37 +109,71 @@ namespace WindowsFormsApplication4
 
         private void button1_Click(object sender, EventArgs e) //New game
         {
+            for (int x = 0; x < numvak; x++)
+            {
+                for (int y = 0; y < numvak; y++ )
+                {
+                    tegels[x, y] = 0;
+                }
+                    
+            }
+            speleraanzet = "Blauw";
+            panel1.Invalidate();
 
         }
 
         private void button3_Click(object sender, EventArgs e) //Help
         {
-
+            
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e) // textbox voor de rode cirkels --> auto-update
-        {
+      
 
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e) // textbox voor de blauwe cirkels --> auto-update
-        {
-
-        }
-
+        
         private void panel1_Paint_1(object sender, PaintEventArgs e)
         {
 
         }
 
-        private void panel3_Paint(object sender, PaintEventArgs e)
+        private void panel3_Paint(object sender, PaintEventArgs e) //panel-positie voor tussenstand rode cirkels1
         {
-            e.Graphics.FillEllipse(Brushes.Red, 0, 0, 50, 50);
+            e.Graphics.FillEllipse(Brushes.Red, 0, 0, 50, 50); 
         }
 
-        private void panel4_Paint(object sender, PaintEventArgs e)
+        private void panel4_Paint(object sender, PaintEventArgs e) //panel-positie voor tussenstand blauwe cirkels 
         {
             e.Graphics.FillEllipse(Brushes.Blue, 0, 0, 50, 50);
         }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Invalidate();
+        }
+
+       void Label1_Teller()
+        {
+            int teller1 = 0;
+            int teller2 = 0;
+            for(int x = 0; x < numvak; x++)
+            {
+                for (int y = 0; y < numvak; y++)
+                {
+                    if (tegels[x, y] == 1)
+                    {
+                        teller1++;
+
+                    }
+                    if (tegels[x, y] == -1)
+                    {
+                        teller2++;
+                    }
+                }
+            }
+            label1.Text = teller1.ToString();
+            label2.Text = teller2.ToString();
+
+        }
+        
+      
     }
 }
